420b7dc3c78224bdbd8fa7bcee849e716c9fdd53
RyanLanz123 <ryanlanz6@gmail.com>
2025-01-03 11:10:36 +0000

    add login css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
+import './Login.css';
+import logo from '../../assets/logo.png';
 
 const Login = () => {
   return (
-    <div className='Login'>
-      
+    <div className="login">
+      <img src={logo} className="login-logo" alt="Logo" />
+      <div className="login-form">
+        <h1>Sign In</h1>
+        <form>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Sign In</button>
+          {/* Remember Me and Need Help Section */}
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" id="rememberMe" />
+              <label htmlFor="rememberMe">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
